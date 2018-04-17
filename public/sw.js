395151c79ff0a612fde1c33a3beff29f174b1539
helenaleivas <lennaleivas@gmail.com>
2018-04-17 16:59:34 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,33 +1,71 @@
-//This is the "Offline page" service worker
-
-//Install stage sets up the offline page in the cahche and opens a new cache
-self.addEventListener('install', function(event) {
-  var offlinePage = new Request('offline.html');
+//NEW
+//This is the "Offline copy of pages" wervice worker
+//Install stage sets up the index page (home page) in the cahche and opens a new cache
+self.addEventListener('install', function (event) {
+  var indexPage = new Request('index.html');
   event.waitUntil(
-  fetch(offlinePage).then(function(response) {
-    return caches.open('pwabuilder-offline').then(function(cache) {
-      console.log('[PWA Builder] Cached offline page during Install'+ response.url);
-      return cache.put(offlinePage, response);
-    });
-  }));
+      fetch(indexPage).then(function (response) {
+          caches.open('pwabuilder-offline').then(function (cache) {
+              console.log('[PWA Builder] Cached index page during Install' + response.url);
+              return cache.addAll(['/final/public/', '/final/public/index.html', '/final/public/bebida.html',
+                  '/final/public/bolo.html', '/final/public/cardapio.html',
+                  '/final/public/cadastro.html', '/final/public/cadastro2.html',
+                  '/final/public/cadastro3.html', '/final/public/contato.html',
+                  '/final/public/carrinho.html', '/final/public/doce.html',
+                  '/final/public/login.html', '/final/public/final.html',
+                  '/final/public/perfil.html', '/final/public/finalizando.html',
+                  '/final/public/perfil2.html', '/final/public/local.html',
+                  '/final/public/politica.html',
+                  '/final/public/produto.html', '/final/public/termos.html',
+                  '/final/public/salgado.html', '/final/public/salvo.html',
+                  '/final/public/visitante.html', '/final/public/sobre.html',
+                  '/final/public/css/style.css', '/final/public/sw.js',
+                  '/final/public/doce/doce.jpeg', '/final/public/doce/doce2.jpeg',
+                  '/final/public/doce/doce3.jpeg', '/final/public/doce/doce4.jpeg',
+                  '/final/public/doce/doce5.jpeg', '/final/public/doce/doce6.jpeg',
+                  '/final/public/doce/doce7.jpeg', '/final/public/doce/doce8.jpeg',
+                  '/final/public/doce/doce9.jpeg', '/final/public/doce/doce10.jpeg',
+                  '/final/public/doce/doce11.jpeg', '/final/public/doce/doce12.jpeg',
+                  '/final/public/salgado/salgado1.jpeg', '/final/public/salgado/salgado2.jpeg',
+                  '/final/public/salgado/salgado3.jpeg', '/final/public/salgado/salgado4.jpeg',                 
+                  '/final/public/salgado/salgado5.jpeg', '/final/public/salgado/salgado6.jpeg',
+                  '/final/public/salgado/salgado7.jpeg', '/final/public/salgado/salgado8.jpeg',
+                  '/final/public/salgado/salgado9.jpeg', '/final/public/salgado/salgado10.jpeg',
+                  '/final/public/salgado/salgado11.jpeg', '/final/public/salgado/salgado12.jpeg',
+                  '/final/public/salgado/risoles.jpeg', '/final/public/salgado/tortaFrango.jpeg',                 
+              ]);
+          });
+      })
+  );
 });
 
-//If any fetch fails, it will show the offline page.
-//Maybe this should be limited to HTML documents?
+
+//If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-        console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          return cache.match('offline.html');
-      });
-    }));
-});
-
-//This is a event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener('refreshOffline', function(response) {
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
-    return cache.put(offlinePage, response);
+var updateCache = function(request){
+  return caches.open('pwabuilder-offline').then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log('[PWA Builder] add page to offline'+response.url)
+      return cache.put(request, response);
+    });
   });
-});
+};
+
+event.waitUntil(updateCache(event.request));
+
+event.respondWith(
+  fetch(event.request).catch(function(error) {
+    console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+
+    //Check to see if you have it in the cache
+    //Return response
+    //If not in the cache, then return error page
+    return caches.open('pwabuilder-offline').then(function (cache) {
+      return cache.match(event.request).then(function (matching) {
+        var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+        return report
+      });
+    });
+  })
+);
+})
