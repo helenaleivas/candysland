88f0545c0f949302c48822daecb8553dd4f46f60
helenaleivas <lennaleivas@gmail.com>
2018-04-22 21:40:59 -0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,20 @@
 //NEW
 //This is the "Offline copy of pages" wervice worker
 //Install stage sets up the index page (home page) in the cahche and opens a new cache
+
+//Google Maps da pagina de "localização" (local.html)
+function initMap() {
+  var ifrs = {lat: -32.04064, lng: -52.088499};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: ifrs
+  });
+  var marker = new google.maps.Marker({
+    position:ifrs,
+    map: map
+  });
+}
+//Service Work
 self.addEventListener('install', function (event) {
   var indexPage = new Request('index.html');
   event.waitUntil(
